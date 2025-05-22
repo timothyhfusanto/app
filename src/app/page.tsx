7e269a68fcf1938a7277876626faae5842004297
Timothy Fusanto <e1070560@u.nus.edu>
2025-05-23 04:04:00 +0800

    add information on hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,16 @@ import Speakers from "./speakers";
 import Registration from "./registration";
 import Venue from "./venue";
 import { motion } from "framer-motion";
+import { Card } from "@/components/ui/card";
+import { ArrowRight, Calendar, ChevronRight, Clock, MapPin } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <section
         id="home"
-        className="relative min-h-screen w-full flex items-center justify-center"
+        className="relative min-h-screen w-full flex items-center justify-center "
       >
         {/* Background Image */}
         <Image
@@ -26,7 +29,7 @@ export default function Home() {
 
         {/* Overlay Content */}
         <div
-          className="z-10 text-center px-4 text-primary flex flex-col gap-6 items-center justify-center h-full uppercase"
+          className="z-10 text-center container mx-auto py-20 px-10 text-primary flex flex-col gap-10 items-center justify-center h-full uppercase"
 
         >
           <motion.p
@@ -39,23 +42,51 @@ export default function Home() {
             Singapore , National University of Singapore
           </motion.p>
           <motion.h1
-            className="text-5xl lg:text-7xl max-w-4xl font-bold mb-4 leading-normal uppercase tracking-tight"
+            className="text-5xl lg:text-7xl max-w-4xl font-bold leading-tight uppercase tracking-tight"
             initial={{ opacity: 0, y: -50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.6 }}
             viewport={{ once: true }}
           >
-            Impact of Information <br /> Systems <span className="text-secondary">Research</span>
+            Impact of Information <br /><span className="text-secondary">Systems Research</span>
           </motion.h1>
-          <motion.p
-            className="text-lg lg:text-2xl font-semibold"
+          <motion.div
+            className="text-sm lg:text-xl font-semibold flex flex-col md:flex-row gap-4"
             initial={{ opacity: 0, y: -50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.4, duration: 0.6 }}
             viewport={{ once: true }}
           >
-            sat, 2 Jul, 2025
-          </motion.p>
+            <Card className="rounded-lg shadow-lg flex-row bg-white/10 backdrop-blur-sm border-none text-primary p-4 flex items-center gap-x-4">
+              <Calendar />
+              <p className="text-center">
+                Sat, 2 Jul, 2025
+              </p>
+            </Card><Card className="rounded-lg shadow-lg flex-row bg-white/10 backdrop-blur-sm border-none text-primary p-4 flex items-center gap-x-4">
+              <Clock />
+              <p className="text-center">
+                9:00 AM - 5:00 PM
+              </p>
+            </Card>
+            <Card className="rounded-lg shadow-lg flex-row bg-white/10 backdrop-blur-sm border-none text-primary p-4 flex items-center gap-x-4">
+              <MapPin />
+              <p className="text-center">
+                NUS, Singapore
+              </p>
+            </Card>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.6, duration: 0.6 }}
+            viewport={{ once: true }}
+          >
+            <Button
+              className="px-10 py-8 text-md md:text-xl cursor-pointer relative overflow-hidden group bg-secondary hover:bg-white hover:text-black"
+            >
+              REGISTER NOW <span><ChevronRight className="transition-transform group-hover:translate-x-2" /></span>
+            </Button>
+          </motion.div>
         </div>
       </section>
       <About />
