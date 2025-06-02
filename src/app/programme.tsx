e88da9fc02d68b367556d2c212af96c0b3db356c
Timothy Fusanto <e1070560@u.nus.edu>
2025-06-02 18:20:38 +0700

    small fix

--- src/app/programme.tsx
+++ src/app/programme.tsx
@@ -80,7 +80,7 @@ export default function Programme() {
 							<motion.div
 								initial={{ opacity: 0, y: -50 }}
 								whileInView={{ opacity: 1, y: 0 }}
-								transition={{ delay: index * 0.1, duration: 0.8 }}
+								transition={{ delay: 0.2, duration: 0.8 }}
 								viewport={{ once: true }}
 								key={index}
 							>
